d69f2c549fc980c959ee1f1838f59d175ed3d79b
katendejericho <106674175+katendejericho5@users.noreply.github.com>
2024-08-31 22:39:30 +0300

    anyone can acess this

--- user_auth_api/auth_with_jwt.py
+++ user_auth_api/auth_with_jwt.py
@@ -1,46 +1,14 @@
+from flask import Flask, request, jsonify, make_response, render_template, session, flash
 import jwt
-from flask import Flask, request, jsonify, make_response
-import datetime
+from datetime import datetime, timedelta
+from functools import wraps
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'KEEP_IT_A_SECRET'
 
-app.config['secret_key'] = "this is secret"
+@app.route('/public')
+def public():
+ return 'Anyone can access this'
 
-
-def token_required(f):
-    def decorated(*args, **kwargs):
-        token = request.args.get('token')
-        if not token:
-            return jsonify({'error': 'token is missing'}), 403
-        try:
-            jwt.decode(token, app.config['secret_key'], algorithms=["HS256"])
-        except jwt.ExpiredSignatureError:
-            return jsonify({'error': 'token has expired'}), 403
-        except jwt.InvalidTokenError:
-            return jsonify({'error': 'token is invalid'}), 403
-        return f(*args, **kwargs)
-    return decorated
-
-
-@app.route("/login")
-def login():
-    auth = request.authorization
-    if auth and auth.password == "password":
-        token = jwt.encode({'user': auth.username, 'exp': datetime.datetime.utcnow(
-        ) + datetime.timedelta(seconds=10)}, app.config['secret_key'], algorithm="HS256")
-        
-        # Convert token to string if it's in bytes
-        token_str = token if isinstance(token, str) else token.decode('utf-8')
-        
-        return f'<a href="http://localhost:5001/access?token={token_str}">Private link</a>'
-    return make_response('Could not Verify', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
-
-
-@app.route("/access")
-@token_required
-def access():
-    return jsonify({'message': 'valid jwt token'})
-
-
-if __name__ == "__main__":
-    app.run(port=5002)
+if __name__ == '__main__':
+ app.run(debug=True)
